647efe9c67827543a95ba67052e397a6f93767b0
Kieran Desoer <kieran.desoer1@engineering.digital.dwp.gov.uk>
2024-06-17 11:37:45 +0100

    Complete exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let myString = "";
+    for (i=0; i<times; i++){
+        myString += word;
+    }
+    return myString;
 };
 
 // Do not edit below this line

d9e076523e3600fdf6a6fcce732579b9c90473b5
Kieran Desoer <kieran.desoer1@engineering.digital.dwp.gov.uk>
2024-06-17 12:12:26 +0100

    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+    let finalSum = 0;
+    
+    if (!Number.isInteger(firstNum) || !Number.isInteger(lastNum)) return "ERROR";
+    if (firstNum < 0 || lastNum < 0) return "ERROR";
+    
+    if (firstNum > lastNum){
+        let tempNum = lastNum;
+        lastNum = firstNum;
+        firstNum = tempNum;
+    }
 
+    for (i=firstNum; i<=lastNum; i++){
+        finalSum += i;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
